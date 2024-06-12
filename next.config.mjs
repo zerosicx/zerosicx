cc14fb8b1a5721ee5f375c2191bc64d816cb0b8b
zerosicx <hcsofteng@gmail.com>
2024-06-13 00:53:01 +1200

    Simplified next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-const productionConfig = {
-  output: "export",
+const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
 };
-
-const devConfig = {
-  images: {
-    domains: ['res.cloudinary.com'],
-  },
-};
-
-const nextConfig = process.env.NODE_ENV === "production" ? productionConfig : devConfig;
 
 export default nextConfig;
